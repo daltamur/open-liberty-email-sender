876671b7c938e0b6d9767bc1fdf2c364e99bbd13
daltamur <daltamur@oswego.edu>
2023-03-31 22:44:13 -0400

    initial commit

---
+++ src/main/java/com/email/rest/Resource.java
@@ -0,0 +1,55 @@
+package com.email.rest;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+@Path("send")
+public class Resource {
+
+    @POST
+    public Response getProperties() throws MessagingException, UnsupportedEncodingException {
+        String to="<receiver email>";
+        String from="<sender email>";
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        Session session = Session.getInstance(prop, new jakarta.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("<receiver email>", "<app password>");
+
+            }
+
+        });
+
+        String msgBody = "This is how you send mail through a docker container";
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(from, "NoReply"));
+            msg.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+            msg.setSubject("Here's an example");
+            msg.setText(msgBody);
+            Transport.send(msg);
+            System.out.println("Email sent successfully...");
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Response.ok().build();
+    }
+
+}
